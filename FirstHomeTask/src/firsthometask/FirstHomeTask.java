2047d44de04fd8930873247cf7374fc3443f5102
Galina Novitskaya <galina@loopme.com>
2017-04-27 13:02:23 +0300

    first

---
+++ FirstHomeTask/src/firsthometask/FirstHomeTask.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthometask;
+
+/**
+ *
+ * @author galinanovitskaya
+ */
+public class FirstHomeTask {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Первое домашнее задание от 26.04.2017 \nКлассы-оболочки и примитивные типы");
+        System.out.println("\nПримитивный тип byte и класс-оболочка Byte:"
+        + "\nByte.BYTES = " + Byte.BYTES
+        +"\nByte.MAX_VALUE = " + Byte.MAX_VALUE
+        + "\nByte.MIN_VALUE = " + Byte.MIN_VALUE 
+        + "\nByte.SIZE = " + Byte.SIZE);
+        
+        System.out.println("\nПримитивный тип short и класс-оболочка Short:"
+        + "\nShort.BYTES = " + Short.BYTES
+        + "\nShort.MAX_VALUE = " + Short.MAX_VALUE
+        + "\nShort.MIN_VALUE = " + Short.MIN_VALUE
+        + "\nShort.SIZE = " + Short.SIZE);
+        
+        System.out.println("\nПримитивный тип ing и класс-оболочка Integer:"
+        + "\nInteger.BYTES = " + Integer.BYTES
+        + "\nInteger.MAX_VALUE = " + Integer.MAX_VALUE
+        + "\nInteger.MIN_VALUE = " + Integer.MIN_VALUE
+        + "\nInteger.SIZE = " + Integer.SIZE);
+        
+        System.out.println("\nПримитивный тип long и класс-оболочка Long:"
+        + "\nLong.BYTES  = " + Long.BYTES
+        + "\nLong.MAX_VALUE = " + Long.MAX_VALUE
+        + "\nLong.MIN_VALUE = " + Long.MIN_VALUE
+        + "\nLong.SIZE = " + Long.SIZE);
+        
+        System.out.println("\nПримитивный тип float и класс-оболочка Float:"
+        + "\nFloat.BYTES = " + Float.BYTES
+        + "\nFloat.MAX_EXPONENT = " + Float.MAX_EXPONENT
+        + "\nFloat.MAX_VALUE = " + Float.MAX_VALUE
+        + "\nFloat.MIN_EXPONENT = " + Float.MIN_EXPONENT
+        + "\nFloat.MIN_NORMAL = " + Float.MIN_NORMAL
+        + "\nFloat.MIN_VALUE = " + Float.MIN_VALUE
+        + "\nFloat.NEGATIVE_INFINITY = " + Float.NEGATIVE_INFINITY
+        + "\nFloat.NaN = " + Float.NaN
+        + "\nFloat.POSITIVE_INFINITY = " + Float.POSITIVE_INFINITY
+        + "\nFloat.SIZE = " + Float.SIZE);
+        
+        System.out.println("\nПримитивный тип double и класс-оболочка Double:"
+        + "\nDouble.BYTES = " + Double.BYTES
+        + "\nDouble.MAX_EXPONENT = " + Double.MAX_EXPONENT
+        + "\nDouble.MAX_VALUE = " + Double.MAX_VALUE
+        + "\nDouble.MIN_EXPONENT = " + Double.MIN_EXPONENT
+        + "\nDouble.MIN_NORMAL = " + Double.MIN_NORMAL
+        + "\nDouble.MIN_VALUE = " + Double.MIN_VALUE
+        + "\nDouble.NEGATIVE_INFINITY = " + Double.NEGATIVE_INFINITY
+        + "\nDouble.NaN = " + Double.NaN
+        + "\nDouble.POSITIVE_INFINITY = " + Double.POSITIVE_INFINITY
+        + "\nDouble.SIZE = " + Double.SIZE);
+        
+
+        System.out.println("\nПримитивный тип boolean и класс-оболочка Doolean:"
+        + "\nBoolean.FALSE = " + Boolean.FALSE
+        + "\nBoolean.TRUE = " + Boolean.TRUE);
+
+        System.out.println("\nПримитивный тип char и класс-оболочка Character:"
+        + "\nCharacter.BYTES = " + Character.BYTES
+        + "\nCharacter.CONNECTOR_PUNCTUATION = " + Character.CONNECTOR_PUNCTUATION
+        + "\nCharacter.COMBINING_SPACING_MARK = " + Character.COMBINING_SPACING_MARK
+        + "\nCharacter.CONTROL = " + Character.CONTROL
+        + "\nCharacter.CURRENCY_SYMBOL = " + Character.CURRENCY_SYMBOL
+        + "\nCharacter.DASH_PUNCTUATION = " + Character.DASH_PUNCTUATION
+        + "\nCharacter.DECIMAL_DIGIT_NUMBER = " + Character.DECIMAL_DIGIT_NUMBER
+        + "\nCharacter.DIRECTIONALITY_ARABIC_NUMBER = " + Character.DIRECTIONALITY_ARABIC_NUMBER
+        + "\nCharacter.DIRECTIONALITY_BOUNDARY_NEUTRAL = " + Character.DIRECTIONALITY_BOUNDARY_NEUTRAL
+        + "\nCharacter.DIRECTIONALITY_COMMON_NUMBER_SEPARATOR = " + Character.DIRECTIONALITY_COMMON_NUMBER_SEPARATOR
+        + "\nCharacter.DIRECTIONALITY_EUROPEAN_NUMBER = " + Character.DIRECTIONALITY_EUROPEAN_NUMBER
+        + "\nCharacter.DIRECTIONALITY_EUROPEAN_NUMBER_SEPARATOR = " + Character.DIRECTIONALITY_EUROPEAN_NUMBER_SEPARATOR
+        + "\nCharacter.DIRECTIONALITY_EUROPEAN_NUMBER_TERMINATOR = " + Character.DIRECTIONALITY_EUROPEAN_NUMBER_TERMINATOR
+        + "\nCharacter.DIRECTIONALITY_LEFT_TO_RIGHT = " + Character.DIRECTIONALITY_LEFT_TO_RIGHT
+        + "\nCharacter.DIRECTIONALITY_LEFT_TO_RIGHT_EMBEDDING = " + Character.DIRECTIONALITY_LEFT_TO_RIGHT_EMBEDDING        
+        + "\nCharacter.DIRECTIONALITY_LEFT_TO_RIGHT_OVERRIDE = " + Character.DIRECTIONALITY_LEFT_TO_RIGHT_OVERRIDE
+        + "\nCharacter.DIRECTIONALITY_NONSPACING_MARK = " + Character.DIRECTIONALITY_NONSPACING_MARK        
+        + "\nCharacter.DIRECTIONALITY_OTHER_NEUTRALS = " + Character.DIRECTIONALITY_OTHER_NEUTRALS
+        + "\nCharacter.DIRECTIONALITY_PARAGRAPH_SEPARATOR = " + Character.DIRECTIONALITY_PARAGRAPH_SEPARATOR
+        + "\nCharacter.DIRECTIONALITY_POP_DIRECTIONAL_FORMAT = " + Character.DIRECTIONALITY_POP_DIRECTIONAL_FORMAT
+        + "\nCharacter.DIRECTIONALITY_RIGHT_TO_LEFT = " + Character.DIRECTIONALITY_RIGHT_TO_LEFT
+        + "\nCharacter.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC = " + Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC
+        + "\nCharacter.DIRECTIONALITY_RIGHT_TO_LEFT_EMBEDDING = " + Character.DIRECTIONALITY_RIGHT_TO_LEFT_EMBEDDING
+        + "\nCharacter.DIRECTIONALITY_RIGHT_TO_LEFT_OVERRIDE = " + Character.DIRECTIONALITY_RIGHT_TO_LEFT_OVERRIDE
+        + "\nCharacter.DIRECTIONALITY_SEGMENT_SEPARATOR = " + Character.DIRECTIONALITY_SEGMENT_SEPARATOR
+        + "\nCharacter.DIRECTIONALITY_UNDEFINED = " + Character.DIRECTIONALITY_UNDEFINED
+        + "\nCharacter.DIRECTIONALITY_WHITESPACE = " + Character.DIRECTIONALITY_WHITESPACE
+        + "\nCharacter.ENCLOSING_MARK = " + Character.ENCLOSING_MARK
+        + "\nCharacter.END_PUNCTUATION = " + Character.END_PUNCTUATION
+        + "\nCharacter.FINAL_QUOTE_PUNCTUATION = " + Character.FINAL_QUOTE_PUNCTUATION
+        + "\nCharacter.FORMAT = " + Character.FORMAT
+        + "\nCharacter.INITIAL_QUOTE_PUNCTUATION = " + Character.INITIAL_QUOTE_PUNCTUATION
+        + "\nCharacter.LETTER_NUMBER = " + Character.LETTER_NUMBER
+        + "\nCharacter.LINE_SEPARATOR = " + Character.LINE_SEPARATOR
+        + "\nCharacter.LOWERCASE_LETTER = " + Character.LOWERCASE_LETTER
+        + "\nCharacter.MATH_SYMBOL = " + Character.MATH_SYMBOL
+        + "\nCharacter.MAX_CODE_POINT = " + Character.MAX_CODE_POINT
+        + "\nCharacter.MAX_HIGH_SURROGATE = " + Character.MAX_HIGH_SURROGATE
+        + "\nCharacter.MAX_LOW_SURROGATE = " + Character.MAX_LOW_SURROGATE
+        + "\nCharacter.MAX_RADIX = " + Character.MAX_RADIX
+        + "\nCharacter.MAX_SURROGATE = " + Character.MAX_SURROGATE
+        + "\nCharacter.MAX_VALUE = " + Character.MAX_VALUE
+        + "\nCharacter.MIN_CODE_POINT = " + Character.MIN_CODE_POINT
+        + "\nCharacter.MIN_HIGH_SURROGATE = " + Character.MIN_HIGH_SURROGATE
+        + "\nCharacter.MIN_LOW_SURROGATE = " + Character.MIN_LOW_SURROGATE
+        + "\nCharacter.MIN_RADIX = " + Character.MIN_RADIX
+        + "\nCharacter.MIN_SUPPLEMENTARY_CODE_POINT = " + Character.MIN_SUPPLEMENTARY_CODE_POINT
+        + "\nCharacter.MIN_SURROGATE = " + Character.MIN_SURROGATE
+        + "\nCharacter.MIN_VALUE = " + Character.MIN_VALUE
+        + "\nCharacter.MODIFIER_LETTER = " + Character.MODIFIER_LETTER
+        + "\nCharacter.MODIFIER_SYMBOL = " + Character.MODIFIER_SYMBOL
+        + "\nCharacter.NON_SPACING_MARK = " + Character.NON_SPACING_MARK
+        + "\nCharacter.OTHER_LETTER = " + Character.OTHER_LETTER
+        + "\nCharacter.OTHER_NUMBER = " + Character.OTHER_NUMBER
+        + "\nCharacter.OTHER_PUNCTUATION = " + Character.OTHER_PUNCTUATION
+        + "\nCharacter.OTHER_SYMBOL = " + Character.OTHER_SYMBOL
+        + "\nCharacter.PARAGRAPH_SEPARATOR = " + Character.PARAGRAPH_SEPARATOR
+        + "\nCharacter.PRIVATE_USE = " + Character.PRIVATE_USE
+        + "\nCharacter.SIZE = " + Character.SIZE
+        + "\nCharacter.SPACE_SEPARATOR = " + Character.SPACE_SEPARATOR
+        + "\nCharacter.START_PUNCTUATION = " + Character.START_PUNCTUATION
+        + "\nCharacter.SURROGATE = " + Character.SURROGATE
+        + "\nCharacter.TITLECASE_LETTER = " + Character.TITLECASE_LETTER
+        + "\nCharacter.UNASSIGNED = " + Character.UNASSIGNED
+        + "\nCharacter.UPPERCASE_LETTER = " + Character.UPPERCASE_LETTER
+        );
+        
+        System.out.println("\nИнформация по классу Math:"
+        + "\nMath.E = " + Math.E
+        + "\nMath.PI = " + Math.PI);
+     
+    }
+    
+}
